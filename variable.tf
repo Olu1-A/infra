6a0c4137a3a4088460b141c17d8942b7b2fdc378
Olugbenga Agbeluyi <agbeluyigbenga@gmail.com>
2023-03-17 18:24:16 -0600

    corrected as discussed

--- variable.tf
+++ variable.tf
@@ -40,9 +40,9 @@ variable "instance_type_list" {
 variable "instance_type_map" {
   description = "EC2 Instance Type"
   type = map(string)
-}
   default = {
     "dev" = "t3.micro"
     "qa" = "t3.small"
     "prod" = "t3.large"
   }
+}
